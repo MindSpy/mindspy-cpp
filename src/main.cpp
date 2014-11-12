ecfec858fa8c034d58d7f2bd0a65f180e40a3d14
Peter Boraros <pborky@pborky.sk>
2014-11-12 02:11:34 +0100

    test and use cases

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,28 @@
 #include "CodedStream.hpp"
 #include "Subprocess.hpp"
 #include "MatchingStream.hpp"
+#include "Proto.hpp"
 
 namespace mindspy {
 namespace test {
 
 using namespace google::protobuf;
-
+using namespace protobufs;
 
 int main() {
     Subprocess sub("../firmware/test/server");
     CodedStream cs(sub.ifd(), sub.ofd());
     MatchingStream ms(cs);
+
+    Request req;
+    Response resp;
+
+    // TODO set request
+
+    ms.put(req);
+
+    ms.get(resp);
+
     // TODO ..
 }
 
