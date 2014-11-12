2f7ad22289bdbf27c12e108c9f746f2ab1a4e1dd
razor <maficccc@gmail.com>
2014-11-12 18:12:15 +0100

    add check usb device

--- src/Usbstream.cpp
+++ src/Usbstream.cpp
@@ -10,18 +10,24 @@ UsbStream::UsbStream() : deviceContext(nullptr), numberOfDevices(0)
     if(numberOfDevices < 0)
     {
         // if return more than 0 else libusb error
+        return 1;
     }
 
     libusb_set_debug(deviceContext, LOG_LEVEL_USB::LOG_WARNING);
 
-
-
-
+    // Get device
+    deviceContext = libusb_get_device(deviceContext, &devices);
+    if(deviceContext < 0)
+    {
+        // not found device
+        return 1;
+    }
 
 }
 
 UsbStream::~UsbStream()
 {
+    // Deinicialize library
     libusb_exit(deviceContext);
 }
 
