cff6288bd22c936a0c2c32f4c197bb3df8f9494e
razor <maficccc@gmail.com>
2014-11-14 20:26:21 +0100

    Complete Queue class

--- src/Queue.hpp
+++ src/Queue.hpp
@@ -17,33 +17,79 @@ class Queue
 {
 public:
 
-
+    // Defined default constructor.
     Queue() = default;
-    // Disable copy and assignment.
+    // Disable copy constructor.
     Queue(const &Queue) = delete;
+    // Disable assignment constructor.
     Queue& operator=(const Queue&) = delete;
 
     /*!
      * Remove element from queue.
      */
-    void pop();
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+
+        while(queue.empty())
+        {
+            condicion.wait(lock);
+        }
+
+        auto element = queue.front();
+
+        queue.pop();
+
+        return element;
+    }
+
+    /*!
+     * Remove element from queue.
+     */
+    void pop(T& element)
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+
+        while(queue.empty())
+        {
+            condicion.wait(lock);
+        }
+
+        element = queue.front();
+
+        queue.pop();
+    }
 
     /*!
      * Add element to queue.
      */
-    void push();
+    void push(T& element)
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
 
-    /*!
-     * Checking empty queue.
-     * \return bool value according to stage queue.
-     */
-    bool empty() const;
+        queue.push(element);
+
+        lock.unlock();
+
+        condicion.notify_one();
+    }
+
+    //void push(T&& element)
+    //{
+    //    std::unique_lock<std::mutex> lock(queueMutex);
+    //
+    //    queue.push(std::move(element));
+    //
+    //    lock.unlock();
+    //
+    //     condicion.notify_one();
+    //}
 
 private:
 
     std::queue<T> queue;
     std::mutex queueMutex;
-    std::condition_variable queueCondicion;
+    std::condition_variable condicion;
 };
 
 } // namespace
